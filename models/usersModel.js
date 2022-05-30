ae0e852e7491a51c74c2a90a9dba8d678ca89be0
Ascended300 <eduardofelix014@gmail.com>
2022-05-30 14:53:41 -0300

    Comunicación con la base de datos local lograda

---
+++ models/usersModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        min: 3,
+        max: 20,
+        unique: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        max: 50,
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: true,
+        min: 8,
+    },
+    isAvatarImageSet: {
+        type: Boolean,
+        default: false,
+    },
+    avatarImage: {
+        type: String,
+        default: "",
+    }
+})
+
+module.exports = mongoose.model('Users', userSchema)
