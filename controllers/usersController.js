663afdba3e0ecb4841a453886d1efd4d9f1c591c
Ascended300 <eduardofelix014@gmail.com>
2022-05-30 16:13:31 -0300

    Login funcionando con base de datos incluida

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -25,3 +25,22 @@ module.exports.register = async (req, res, next) => {
     }
     
 }
+
+module.exports.login = async (req, res, next) => {
+    try{
+        const { username, password } = req.body
+        const user = await User.findOne({username})
+        if(!user){
+            return res.json({msg:'Incorrect username or passsword', status: false})
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if(!isPasswordValid){
+            return res.json({msg:'Incorrect username or passsword', status: false})
+        }
+        delete user.password
+        return res.json({status: true, user})
+    } catch(err) {
+        next(err)
+    }
+    
+}
