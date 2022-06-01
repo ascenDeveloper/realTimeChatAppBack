957bacae131f1362fea2b4624614d19a9b1efbf7
Ascended300 <eduardofelix014@gmail.com>
2022-06-01 10:14:03 -0300

    Añadido el setAvatar a las rutas y controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -44,3 +44,18 @@ module.exports.login = async (req, res, next) => {
     }
     
 }
+
+module.exports.setAvatar = async (req, res, next) => {
+    try{
+        const userId = req.params.id
+        const avatarImage = req.body.image
+        const userData = await User.findByIdAndUpdate(userId, {
+            isAvatarImageSet: true,
+            avatarImage
+        })
+        return res.json({ isSet: userData.isAvatarImageSet, image: userData.avatarImage})
+    } catch(err) {
+        next(err)
+    }
+    
+}
